79d3839c175f88c1501b45eabd08c6d695823e37
Ricardo Borges <ricardoborges.ufg@gmail.com>
2015-06-03 22:41:46 -0300

    Continuação da implementação do agente Semaforo.

--- semaforos-inteligentes/src/br/ufg/inf/semaforo/agent/SemaforoAgent.java
+++ semaforos-inteligentes/src/br/ufg/inf/semaforo/agent/SemaforoAgent.java
@@ -2,6 +2,8 @@ package br.ufg.inf.semaforo.agent;
 
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class SemaforoAgent extends Agent {
 
@@ -12,6 +14,7 @@ public class SemaforoAgent extends Agent {
 	
 	public static Integer COUNT_SEMAFOROS = 0;
 	
+	@SuppressWarnings("serial")
 	@Override
 	protected void setup() {
 		System.out.println("Olá! Eu sou um agente Semáforo, meu id é: " + getAID().getName());
@@ -21,7 +24,15 @@ public class SemaforoAgent extends Agent {
 			
 			@Override
 			public void action() {
+				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+				ACLMessage message = myAgent.receive(mt);
 				
+				if (message != null) {
+					// ...
+				} else {
+					//TODO Testar sem o block();
+					block();
+				}
 			}
 		});
 	}
